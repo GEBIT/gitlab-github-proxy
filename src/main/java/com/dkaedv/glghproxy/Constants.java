0d33581f44f0a23bffce9d631aab008a5a0631e1
Carsten Pfeiffer <carsten.pfeiffer@gebit.de>
2017-08-01 09:40:07 +0200

    Add constant for SSL policy

---
+++ src/main/java/com/dkaedv/glghproxy/Constants.java
@@ -0,0 +1,10 @@
+package com.dkaedv.glghproxy;
+
+public final class Constants {
+
+	private Constants() {}
+
+	public static final boolean STRICT_SSL = true;
+	public static final boolean IGNORE_SSL_ERRORS = !STRICT_SSL;
+
+}
